2d7a9b26c595a694bb1f5a343fec78c25169a410
kun zhang <zhang_kun97@163.com>
2018-12-04 18:53:20 +0800

    提交文件

---
+++ users/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            authenticate_user = authenticate(username=new_user.username,
+                                             password=request.POST['password'])
+            login(request, authenticate_user);
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
