7be8f1e5a1f3faa6428e4eba58876d2a16d83405
zhangkun902 <zhang_kun97@163.com>
2020-04-08 14:09:09 +0800

    添加gitignore

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'', include('learning_logs.urls', namespace='learning_logs')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
